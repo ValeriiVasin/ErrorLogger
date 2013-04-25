3bc4071f95bf23751a5f5a708d93899d131909bb
Valeriy Vasin <valeriy.vasin@sup.com>
2013-04-26 00:38:18 +0400

    Basic view of memory type data.

---
+++ app.js
@@ -0,0 +1,27 @@
+angular.module('ErrorLogger');
+
+function ErrorCtrl($scope, $http) {
+
+    // map data
+    var ua = new UAParser();
+    window.data = window.data.slice(0, 200).map(function (data) {
+        data.errors = data.errors.slice(0, 10).map(function (error) {
+            var qs = error.qs,
+                _ua;
+
+            ua.setUA(error.ua);
+            _ua = ua.getResult();
+
+            return {
+                user: qs.user || '<anonymous>',
+                where: qs.where,
+                browser: _ua.browser.name + ' ' + _ua.browser.version,
+                os: _ua.os.name
+            };
+        });
+
+        return data;
+    });
+
+    $scope.logs = window.data;
+}
