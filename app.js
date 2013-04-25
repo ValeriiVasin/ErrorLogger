ad89009918fb11818eadf88561aed7804c0a3374
Valeriy Vasin <valeriy.vasin@sup.com>
2013-04-26 02:04:34 +0400

    Refactor slightly.

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-angular.module('ErrorLogger');
+/*global UAParser*/
+;(function () {
+    'use strict';
+    var app = angular.module('ErrorLogger', []);
 
-function ErrorCtrl($scope, $http) {
+    app.controller('ErrorCtrl', ['$scope', function ($scope) {
+        // map data
+        var ua = new UAParser();
+        window.data = window.data.slice(0, 200).map(function (data) {
+            data.errors = data.errors.slice(0, 10).map(function (error) {
+                var qs = error.qs,
+                    _ua;
 
-    // map data
-    var ua = new UAParser();
-    window.data = window.data.slice(0, 200).map(function (data) {
-        data.errors = data.errors.slice(0, 10).map(function (error) {
-            var qs = error.qs,
-                _ua;
+                ua.setUA(error.ua);
+                _ua = ua.getResult();
 
-            ua.setUA(error.ua);
-            _ua = ua.getResult();
+                return {
+                    user: qs.user || '<anonymous>',
+                    where: qs.where,
+                    browser: _ua.browser.name + ' ' + _ua.browser.version,
+                    os: _ua.os.name
+                };
+            });
 
-            return {
-                user: qs.user || '<anonymous>',
-                where: qs.where,
-                browser: _ua.browser.name + ' ' + _ua.browser.version,
-                os: _ua.os.name
-            };
+            return data;
         });
 
-        return data;
-    });
-
-    $scope.logs = window.data;
-}
+        $scope.logs = window.data;
+    }]);
+}());
